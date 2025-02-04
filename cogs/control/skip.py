5598377e52a1a9afd8df332916e82afc5bd751ef
naberiusClxn <naberius.clxn@gmail.com>
2025-02-04 19:12:37 +0100

    Add files via upload
    Created 2 Cogs lock and skip
    
    lock - removes a role from the config (the role on the server must be configured for display rights)
    
    skip - issues the role from the config (the role on the server must be configured for display rights)
    
    config has been changed 
    added skip_role_id

---
+++ cogs/control/skip.py
@@ -0,0 +1,24 @@
+import disnake
+from disnake.ext import commands
+from config import moderation_role_id
+from config import skip_role_id
+
+class SkipCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.slash_command(description="Выдать доступ к приватным голосовым каналам")
+    async def skip(self, inter: disnake.ApplicationCommandInteraction, user: disnake.Member):
+        if not any(role.id in moderation_role_id for role in inter.author.roles):
+            await inter.response.send_message("У вас нет доступа к этой команде.", ephemeral=True)
+            return
+
+        role = inter.guild.get_role(skip_role_id)
+        if not role:
+            return
+
+        await user.add_roles(role)
+
+
+def setup(bot):
+    bot.add_cog(SkipCog(bot))
